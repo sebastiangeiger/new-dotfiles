021dc76d9e602af57e71ea301a9a125751e40958
Sebastian Geiger <sebastian.geiger@gmail.com>
2014-05-05 11:10:06 -0700

    Installing git, improving install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
-#!/usr/bin/sh
+#!/usr/bin/env bash
+
+shopt -s extglob
+set -o errtrace
+set -o errexit
+
 if [ `uname` = "Linux" ]; then
-  apt-get install -y rake
+  sudo apt-get install -y rake git-core
 fi
 rake install
