68936e8e6645742ffd6bc7cb1e06c92522463da9
Sebastian Geiger <sebastian.geiger@gmail.com>
2014-05-07 11:45:10 -0700

    Switching to fish shell

---
+++ config/fish/config.fish
@@ -0,0 +1,35 @@
+set -x DOTFILES $HOME/dotfiles
+set -x EDITOR "vim"
+set -x PATH /usr/local/bin $PATH
+
+. $HOME/.config/fish/pushover.fish
+
+. $DOTFILES/check_if_files_were_modified.fish
+
+# Alias ==================
+alias ssh-tunnel="ssh -ND 8080"
+alias gti="git"
+alias m="vim"
+alias n="notify"
+alias v="vagrant"
+alias r="rails"
+alias bx="nocorrect bundle exec"
+alias bundle="nocorrect bundle"
+alias gs="git st"
+alias ga="git add"
+alias gap="git ap"
+alias gl="git l"
+alias gh="git h"
+alias gd="git diff"
+alias gfa="git fetch --all"
+alias gf="nocorrect git flow"
+alias feature="gf feature"
+
+alias t="nocorrect todo.sh"
+alias te="vim ~/Dropbox/todo/todo.txt"
+alias diff="colordiff -u"
+alias git-email-personal='git config user.email "sebastian.geiger@gmail.com"'
+alias git-email-mo='git config user.email "sebastian.geiger@mo-stud.io"'
+
+alias wifi-on="networksetup -setairportpower en0 on"
+alias wifi-off="networksetup -setairportpower en0 off"
---
+++ config/fish/functions/fish_prompt.fish
@@ -0,0 +1,49 @@
+function fish_prompt
+  set -l last_status $status
+  set_color $fish_color_cwd
+  echo -n [
+  echo -n (prompt_pwd)
+  echo -n ]
+  if not test $last_status -eq 0
+    set_color $fish_color_error
+  else
+    set_color green
+  end
+  echo -n '$ '
+  set_color normal
+end
+
+function fish_right_prompt
+  __my_git_info
+  set_color red
+  # printf '[%s]' (command rvm current)
+  set_color normal
+end
+
+function __my_git_info
+  set -l repo_info (command git rev-parse --git-dir --is-inside-git-dir --is-bare-repository --is-inside-work-tree --short HEAD ^/dev/null)
+	test -n "$repo_info"; or return
+
+	set -l git_dir         $repo_info[1]
+	set -l inside_gitdir   $repo_info[2]
+	set -l bare_repo       $repo_info[3]
+	set -l inside_worktree $repo_info[4]
+	set -l short_sha
+	if test (count $repo_info) = 5
+		set short_sha $repo_info[5]
+	end
+
+  set branch (command git symbolic-ref -q --short HEAD ^/dev/null)
+  set -l dirty
+	command git diff --no-ext-diff --quiet --exit-code
+	set -l os $status
+	if test $os -ne 0
+		set dirty '*'
+	end
+
+  set_color red
+  printf '%s' $dirty
+  set_color green
+  printf '[%s]' $branch
+  set_color normal
+end
