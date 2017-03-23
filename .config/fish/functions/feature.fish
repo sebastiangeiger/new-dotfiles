7c30528a6d1e7f21c5a0f5d1405db80cc23657f2
Sebastian Geiger <sebastian.geiger@gmail.com>
2017-03-23 14:32:47 -0700

    feature script enforces lowercase

--- .config/fish/functions/feature.fish
+++ .config/fish/functions/feature.fish
@@ -1,4 +1,4 @@
 function feature
-  set -l branch_name (echo $argv | sed -e 's/ /-/g')
+	set -l branch_name (echo $argv | tr '[:upper:]' '[:lower:]' | sed -e 's/ /-/g')
   git checkout -b "sebastiangeiger/$branch_name"
 end
